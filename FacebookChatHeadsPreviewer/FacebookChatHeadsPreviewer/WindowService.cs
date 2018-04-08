de1f148e5ea48dc9808308659b02833ce4fecdd2
Perconey <perconey@gmail.com>
2018-04-08 22:02:32 +0200

    win service

---
+++ FacebookChatHeadsPreviewer/FacebookChatHeadsPreviewer/WindowService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FacebookChatHeadsPreviewer
+{
+    class WindowService
+    //IWindowService cc
+    {
+        public void ShowWindow(object viewModel)
+        {
+            var win = new Window
+            {
+                Content = viewModel
+            };
+            win.Show();
+        }
+    }
+}
